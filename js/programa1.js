f1c80314508f2c411016f5df77172d1a5bef9a93
Andres Ixcajoc <andreslopez2474@gmail.com>
2022-03-11 13:11:49 -0600

    Parrafo y Tamaño Fuente

---
+++ js/programa1.js
@@ -0,0 +1,11 @@
+var x,y;
+ x = prompt("Ingrese el precio del artículo: ");
+ y = prompt("Ingrese cuantas unidades lleva: ");
+
+var resultado = x * y;
+
+alert("Usted debe abonar Q." + resultado + ".00" )
+
+console.log("El precio del articulo ingresado es: " + x);
+console.log("La cantidad de unidades que lleva del articulo son: " + y);
+ console.log("Usted debe abonar Q.", x * y + ".00");
